f39718ccad2e7e94a8c59065ab36c934c3ebedd8
MCarmody <71148703+MCarmody17@users.noreply.github.com>
2022-09-13 12:54:19 +0200

    First push

---
+++ Q1/Hospital1.java
@@ -0,0 +1,62 @@
+import java.util.Iterator;
+import java.util.Objects;
+
+public class Hospital1 extends HospitalBase {
+
+    public Hospital1() {
+        /* Add your code here! */
+
+
+    }
+
+    @Override
+    public boolean addPatient(PatientBase patient) {
+        String[] time = patient.getTime().split(":");
+        String minutes = time[1];
+
+        return false;
+    }
+
+    @Override
+    public Iterator<> iterator() {
+        return new Iterator<type>() {
+
+            @Override
+            public boolean hasNext() {
+                return true // Change this
+            }
+
+            @Override
+            public type next() {
+                return null // Change this
+            }
+
+        };
+    }
+
+    /* Add any extra functions below */
+
+
+    public static void main(String[] args) {
+        /*
+         * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+         * REMOVE THE MAIN METHOD BEFORE SUBMITTING TO THE AUTOGRADER
+         * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+         * The following main method is provided for simple debugging only
+         */
+        var hospital = new Hospital1();
+        var p1 = new Patient("Max", "11:00");
+        var p2 = new Patient("Alex", "13:00");
+        var p3 = new Patient("George", "14:00");
+        hospital.addPatient(p1);
+        hospital.addPatient(p2);
+        hospital.addPatient(p3);
+        var patients = new Patient[] { p1, p2, p3 };
+        int i = 0;
+        for (var patient : hospital) {
+            if (!Objects.equals(patient, patients[i++])) {
+                System.err.println("Wrong patient encountered, check your implementation!");
+            }
+        }
+    }
+}
